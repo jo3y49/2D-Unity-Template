45aa82e316906c6066992fe5244a396ce3e22820
jo3y49 <71416767+jo3y49@users.noreply.github.com>
2024-05-01 19:04:41 -0700

    Added game data files

---
+++ 2D Template/Assets/Scripts/Data/SaveDataClasses.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public int lives;
+    public int health;
+    public int coins;
+    public int level;
+    public int xp;
+    public int score;
+    public IDictionary<Item, int> inventory = new Dictionary<Item, int>();
+    public int inventoryCap;
+    
+}
+[System.Serializable]
+public class StatData
+{
+    public float playtime;
+    public int deaths;
+    public int kills;
+    public int damageDealt;
+    public int damageTaken;
+    public int coinsCollected;
+    public int itemsCollected;
+}
+[System.Serializable]
+public class WorldData
+{
+    public int currentScene = 1;
+    public Vector2 playerPosition = new();
+}
+
+[System.Serializable]
+public class SettingsData
+{
+    
+}
+
+[System.Serializable]
+public class GameData
+{
+    public PlayerData playerData = new();
+    public WorldData worldData = new();
+    public SettingsData settingsData = new();
+
+    public GameData NewGame()
+    {
+        playerData = new();
+        worldData = new();
+
+        return this;
+    }
+}
